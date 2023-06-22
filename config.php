cadee3cdeb34a19d799a842534d3b38ec14cd55b
All amin Sarker <allamindit6720@gmail.com>
2023-06-23 02:46:26 +0600

    commit to main

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+//$adom="https://securedsmcd.com/smartlink/?a=";
+//$aid="201459";
+//$oid="&sm=34592&co=254892&mt=28";
+
+//$accc = $adom . $aid . $oid;
+$accs = "http://www.myloves4u.com/smartlink/?a=205937&sm=35818&co=260376&mt=28&s1";
+
+
+
+?>
